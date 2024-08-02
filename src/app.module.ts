537fc7086722c289a00309b9b7f8302732ca7ab9
Guyoguyes <g.abduba43@gmail.com>
2024-08-02 10:28:10 +0300

    users

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,8 @@ import { DonorsModule } from './donors/donors.module';
 import { DonationsModule } from './donations/donations.module';
 import { RecepientsModule } from './recepients/recepients.module';
 import { EventsModule } from './events/events.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -16,6 +18,8 @@ import { EventsModule } from './events/events.module';
     DonationsModule,
     RecepientsModule,
     EventsModule,
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/users/users.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Get, Param, Post, Patch } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  createUser(@Body() data: CreateUserDto) {
+    return this.usersService.createUser(data);
+  }
+
+  @Get()
+  getAllUsers() {
+    return this.usersService.getAllUsers();
+  }
+
+  @Get(':id')
+  getUserById(@Param('id') id: string) {
+    return this.usersService.getUserById(id);
+  }
+
+  @Patch(':id')
+  updateUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.updateUser(id, updateUserDto);
+  }
+}
